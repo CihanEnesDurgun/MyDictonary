42a233c2ae2486559cc9e8dbda3ca840cac49d49
Cihan Enes DURGUN <cihanenesdurgun@hotmail.com>
2021-02-03 20:39:38 +0300

    Add project files.

---
+++ MyDictonary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(34, "İSTANBUL");
+            dictionary.Add(27, "GAZİANTEP");
+            dictionary.Add(01, "ANKARA");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item + "Getirildi.");
+            }
+        }
+    }
+}
